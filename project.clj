990022804121b798da6f4d48dc1448277d2e2285
Łukasz Korecki <lukasz@korecki.me>
2019-10-21 03:34:06 +0000

    Remove implied dependency on Caliban and Stature.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject nomnom/duckula "0.4.0"
+(defproject nomnom/duckula "0.5.0"
   :description "RPC server (and soon, client), built on top of JSON+Avro+HTTP"
   :url "https://github.com/nomnom-insights/nomnom.duckula"
 
   :dependencies [[org.clojure/clojure "1.10.1"]
+                 [com.stuartsierra/component "0.4.0"]
                  [nomnom/abracad "0.5.0"]]
 
   :plugins [[lein-cloverage "1.0.13" :exclusions [org.clojure/clojure]]]
@@ -10,10 +11,6 @@
   :profiles {:dev
              {:resource-paths ["dev-resources"]
               :dependencies [[org.clojure/tools.logging "0.5.0"]
-                             ;; applications need to provide these!
-                             [nomnom/caliban "1.0.2"]
-                             [nomnom/stature "2.0.0"]
-                             [com.stuartsierra/component "0.4.0"]
                              ;; test & dev deps
                              [clj-http "3.10.0"]
                              [ring-jetty-component "0.3.1"
@@ -21,7 +18,6 @@
                              [org.eclipse.jetty/jetty-server "9.4.21.v20190926"]
                              [ring/ring-defaults "0.3.2"]
                              [ring/ring-json "0.5.0"]
-
                              [compojure "1.6.1"]
                              [ring/ring-mock "0.4.0"
                               :exclusions [ring/ring-codec]]]}})
