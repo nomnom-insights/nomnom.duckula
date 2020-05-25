d460793e2989c4255a0c8a76feec7701c4c685f1
Łukasz Korecki <lukasz@korecki.me>
2020-05-25 11:04:07 -0700

    Release snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nomnom/duckula "0.5.3"
+(defproject nomnom/duckula "0.6.0-SNAPSHOT"
   :description "RPC server (and soon, client), built on top of JSON+Avro+HTTP"
   :url "https://github.com/nomnom-insights/nomnom.duckula"
   :dependencies [[org.clojure/clojure "1.10.1"]
